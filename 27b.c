e772c7efbb0adf950eb3e427c3769ea4d2fc5bbd
sarathpatti <sarathpatti123@gmail.com>
2024-09-21 16:26:14 +0530

    mod

--- 27b.c
+++ 27b.c
@@ -8,45 +8,37 @@ Date: 20 sept, 2024.
 ============================================================================
 */
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <errno.h>
 
-#define MAX 100
-
-struct message_buffer {
+struct msg_buffer {
     long msg_type;
-    char msg_text[MAX];
-};
+    char msg_text[100];
+} message;
 
 int main() {
     key_t key;
     int msgid;
-    struct message_buffer message;
 
-    key = ftok("/tmp", 'a');
-    if (key == -1) {
-        perror("ftok");
-        return 1;
-    }
-
+    key = ftok("progfile", 65);
     msgid = msgget(key, 0666 | IPC_CREAT);
-    if (msgid == -1) {
-        perror("msgget");
-        return 1;
+
+    if (msgrcv(msgid, &message, sizeof(message.msg_text), 0, IPC_NOWAIT) == -1) {
+        if (errno == ENOMSG) {
+            printf("No message available.\n");
+        } else {
+            perror("msgrcv");
+        }
+    } else {
+        printf("Received message: %s\n", message.msg_text);
     }
 
-    if (msgrcv(msgid, &message, sizeof(message.msg_text), 1, IPC_NOWAIT) == -1) {
-        perror("msgrcv");
-        return 1;
-    }
-
-    printf("Received message: %s\n", message.msg_text);
+    msgctl(msgid, IPC_RMID, NULL);
 
     return 0;
 }
-
 /*
-
-msgrcv: No message of desired type
-
+No message available.
 */
